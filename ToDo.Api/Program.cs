fa18d2ac271aee2d27e0c73fb23751885d213371
EngKhaled <khaled.majak123@gmail.com>
2023-03-13 09:50:18 -0700

    addline

--- ToDo.Api/Program.cs
+++ ToDo.Api/Program.cs
@@ -20,8 +20,7 @@ using ToDo.DAL.Extend;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
-//.AddFluentValidation(C=> C.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddControllers().AddFluentValidation(C=> C.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
  
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
